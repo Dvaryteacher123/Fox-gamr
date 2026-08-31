795cdc0f4aceb071020e3e6832e5524eec6ad6d3
Dvaryteacher123 <dullamanyama0@gmail.com>
2026-08-31 16:23:00 +0300

    Add Firebase configuration and initialization
    Set up Firebase configuration and initialization.

---
+++ config/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmwASW4XXQ3O0AvsCM_r1WLlrUmGjYVxI",
+  authDomain: "dvary-9a7d0.firebaseapp.com",
+  projectId: "dvary-9a7d0",
+  storageBucket: "dvary-9a7d0.firebasestorage.app",
+  messagingSenderId: "107370806066",
+  appId: "1:107370806066:web:4c2ce1e6f7b6c32909f52b",
+  measurementId: "G-07361LFJEP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, analytics, db, auth };
+
